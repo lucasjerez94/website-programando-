7f249b388d90b18c7c936bba5ce934dd2e56199a
lucasjerez94 <lucasjerez9468@gmail.com>
2023-12-25 20:38:43 -0300

    css hover

--- src/atoms/Header.jsx
+++ src/atoms/Header.jsx
@@ -1,11 +1,11 @@
 export const Header = () => {
     return(
         <div className="header">
-            <h1 className="h1-header">Programando</h1>
+            <h1 className="h1-header">Programando!</h1>
             <nav className="navbar-header">
                 <ul className="ul-header">
                     <li><a href="/"></a>Inicio</li>
-                    <li><a href="/nosotros"></a>Nosotros</li>
+                    <li><a href="/cursos"></a>Cursos</li>
                     <li><a href="/contacto"></a>Contacto</li>
                 </ul>
             </nav>
